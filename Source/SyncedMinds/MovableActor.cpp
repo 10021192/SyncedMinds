e1179ab7668af52158406de8984fdbe5c5433f09
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2026-01-19 07:15:14 +0000

    Movable Actor Constructor + Moving Platform

--- Source/SyncedMinds/MovableActor.cpp
+++ Source/SyncedMinds/MovableActor.cpp
@@ -9,6 +9,25 @@ AMovableActor::AMovableActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bReplicates = true;
+	SetReplicateMovement(true);
+	
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+	
+	Point1 = CreateDefaultSubobject<UArrowComponent>(TEXT("Point1"));
+	Point1->SetupAttachment(RootComp);
+	Point1->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	
+	Point2 = CreateDefaultSubobject<UArrowComponent>(TEXT("Point2"));
+	Point2->SetupAttachment(RootComp);
+	Point1->SetRelativeLocation(FVector(0.0f, 0.0f, 300.0f));
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComp);
+	Mesh->SetIsReplicated(true);
+	
+	Transporter = CreateDefaultSubobject<UTransporter>(TEXT("Transporter"));
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +35,10 @@ void AMovableActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	FVector StartPoint = GetActorLocation() + Point1->GetRelativeLocation();
+	FVector EndPoint = GetActorLocation() + Point2->GetRelativeLocation();
+	
+	Transporter->SetPoints(StartPoint, EndPoint);
 }
 
 // Called every frame
--- Source/SyncedMinds/MovableActor.h
+++ Source/SyncedMinds/MovableActor.h
@@ -4,6 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Transporter.h"
+
 #include "MovableActor.generated.h"
 
 UCLASS()
@@ -22,5 +27,19 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	USceneComponent* RootComp;
 
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UArrowComponent* Point1;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UArrowComponent* Point2;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UTransporter* Transporter;
 };
