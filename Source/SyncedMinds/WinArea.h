431fa2ce1f5dcd95627481cd7211a98ee01c6d39
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2026-01-19 18:21:20 +0000

    Win Area finished + Win Screen - gameplay loop complete

--- Source/SyncedMinds/WinArea.h
+++ Source/SyncedMinds/WinArea.h
@@ -9,6 +9,8 @@
 
 #include "WinArea.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWinAreaOnWinCondition);
+
 UCLASS()
 class SYNCEDMINDS_API AWinArea : public AActor
 {
@@ -31,4 +33,10 @@ public:
 
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
 	UBoxComponent* WinAreaBox;
+	
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastRPCWin();
+	
+	UPROPERTY(BlueprintAssignable)
+	FWinAreaOnWinCondition OnWinCondition;
 };
