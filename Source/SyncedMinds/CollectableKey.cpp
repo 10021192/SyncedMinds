cb66fc6c4de8fa1ffb62c3ed92f57f870208bf87
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2026-01-19 14:52:01 +0000

    Fix Collectable Key class class order on includes

--- Source/SyncedMinds/CollectableKey.cpp
+++ Source/SyncedMinds/CollectableKey.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Net/UnrealNetwork.h"
-
 #include "CollectableKey.h"
+
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 ACollectableKey::ACollectableKey()
