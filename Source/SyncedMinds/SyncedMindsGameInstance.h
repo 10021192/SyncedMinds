f2ec12a8dbd2ba402a1b6d5551f4efb7dae14c54
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2026-01-26 15:09:49 +0000

    Change matchmaking server IP - 5555/ Change rock implementation
    client still can't move the rockc

--- Source/SyncedMinds/SyncedMindsGameInstance.h
+++ Source/SyncedMinds/SyncedMindsGameInstance.h
@@ -27,8 +27,8 @@ public:
 	
 private:
 	// Matchmaking server configuration
-	FString MatchmakingServerIP = TEXT("192.168.1.64");
-	int32 MatchmakingServerPort = 7777;
+	FString MatchmakingServerIP = TEXT("10.208.152.124");
+	int32 MatchmakingServerPort = 5555;
 
 	// Helper function to request server address from matchmaking
 	FString RequestServerAddress();
